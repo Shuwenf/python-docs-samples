6f07b3688a4831cb8b946fdf8b1308754cd1b9e1
Shuwenf <70035556+Shuwenf@users.noreply.github.com>
2021-11-03 20:38:13 -0500

    updated score section

--- appengine/standard_python3/building-an-app/building-an-app-1/static/snake.js
+++ appengine/standard_python3/building-an-app/building-an-app-1/static/snake.js
@@ -15,9 +15,8 @@ function getInstruction() {
 }
 
 function getRank() {
-  let username = document.getElementById("username").value
   let score = document.getElementById("score").innerHTML
-  location.replace("/rank/" + username + "/" + score)
+  location.replace("/rank/?score=" + score)
 }
 
 function coll(t, e) {
